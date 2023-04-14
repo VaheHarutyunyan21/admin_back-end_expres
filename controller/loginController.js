dac5b91e1dd2ca3e43fbbd766183b871bc16405d
VaheHarutyunyan21 <vaheh@internet.ru>
2023-04-14 18:00:59 +0400

    jwt

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,32 +1,15 @@
 const {User} = require('../models')
 const CryptoJS =require('crypto-js');
-const { json } = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
  
 const SECRET = process.env.SECRET;
 
 
-// function generateAccessToken(users_id,username,isAdmin) {
-//     return jwt.sign({users_id,username,isAdmin},SECRET, { expiresIn: '1h' });
-//   }
-  
-  
-//   exports.loginController = async(req, res) => {
-//       const username = req.body.username
-//       const password = req.body.password
-//        const hashed_password = CryptoJS.SHA256(password).toString();
-//       let sql = "SELECT * from users WHERE username = ?"
-//       await db.get (sql,[username], function(err, row){
-//         if(username == row.username && hashed_password == row.password) {
-//            let token = generateAccessToken(row.users_id,row.username,row.isAdmin)
-//              res.send(JSON.stringify({status:"Logged in",jwt:token}));
-//         }else {
-//             return (res.sendStatus(403))
-//         }
-  
-//       }) 
-//     }
+function generateAccessToken(id,email,isAdmin) {
+    return jwt.sign({id,email,isAdmin},SECRET, { expiresIn: '1h' });
+  }
+
 
 
     exports.loginController = async(req,res)=>{
@@ -36,14 +19,14 @@ const SECRET = process.env.SECRET;
         User.findOne ({ where: { email} })
         .then((prod)=>{
             if (prod.email == email && prod.password == password) {
-                res.status(201).json(prod)
+                let token = generateAccessToken(prod.id,prod.email,prod.isAdmin)
+                console.log(token);
+                 res.status(201).json(token)
+                // res.send(JSON.stringify({status:"Logged in",jwt:token}));
             }else {
-                             return (res.sendStatus(403))
-                        }
+                return (res.sendStatus(403))
+            }
 
-            // console.log(prod);
-            // res.status(201).json(prod)
-            
         }).catch((err)=>{
             res.status(500).json({error: err.message})
         })
