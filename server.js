24f704fca3a906bf2db0efbb7cc6030e0e614067
VaheHarutyunyan21 <vaheh@internet.ru>
2023-04-17 01:22:22 +0400

    Crud

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const auth_routes = require('./routes/auth_routes');
-const users_routes = require('./routes/users_routes')
+const users_routes = require('./routes/users_routes');
+const products_routes = require('./routes/products_routes');
+const categorys_routes = require('./routes/categorys_routes')
+
 
 const app = express();
 app.use(express.json())
@@ -12,6 +15,11 @@ app.use(express.json())
 auth_routes.register_routes(app)
 auth_routes.login_routes(app)
 users_routes.create_users_routes(app)
+products_routes.create_products_routes(app)
+categorys_routes.create_categorys_routes(app)
+
+
+
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
